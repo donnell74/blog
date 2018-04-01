c0440f4430892d826597ec7b1fc3ccb8973eff6d
Owner <owner@Owners-MacBook-Pro.local>
2018-04-01 13:03:01 -0600

    Added Comment model as belongs_to Article.  Also implemented a very simple authentication.  This is the last half of guides.rubyonrails.org/getting_started.html

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+	http_basic_authenticate_with name: "donnell74", password: "secret", except: [:index, :show]
+
 	def index
 		@articles = Article.all
 	end
