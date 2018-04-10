b7e6a75e9033157327ba0f46be6c11161326bd80
Greg Donnell <gregory.donnell74@gmail.com>
2018-04-10 13:52:19 -0600

    Generated sessions logic and added code to log in/out.  Only showing modifying actions if you are logged in.  This code was from the tutorial: https://www.railstutorial.org/book/basic_login

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
-  get 'welcome/index'
+  root   'welcome#index'
+  get    'welcome/index'
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
 
   resources :articles do
   	resources :comments
   end
 
-  root 'welcome#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
