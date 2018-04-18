04aadd2c66ee871f02e7a58c939b3f59583b5131
Greg Donnell <gregory.donnell74@gmail.com>
2018-04-18 16:58:21 -0600

    Add the about me page with a description about myself.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root   'welcome#index'
   get    'welcome/index'
+  get    '/aboutme', to: 'about_me#index'
   get    '/login',   to: 'sessions#new'
   post   '/login',   to: 'sessions#create'
   delete '/logout',  to: 'sessions#destroy'
